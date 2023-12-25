b30bff149099f5eae14a11812368c6884fb340a6
BenS <bshalev1@gmail.com>
2023-12-25 19:12:05 +0200

    home work

--- Divisors.java
+++ Divisors.java
@@ -4,5 +4,14 @@
 public class Divisors {
 	public static void main (String[] args) {
 		//// Put your code here
+		int divisor = Integer.parseInt(args[0]);
+		for (int i = 1; i <= divisor ; i++ ) {
+			if (divisor % i == 0){
+				System.out.println(i);
+
+			}
+			
+
+		}
 	}
 }
