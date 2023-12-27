2c91ca0333f971e4150cc82eb9d7348a32b7136b
BenS <bshalev1@gmail.com>
2023-12-27 16:44:52 +0200

    home work

--- Perfect.java
+++ Perfect.java
@@ -4,5 +4,27 @@
 public class Perfect {
 	public static void main (String[] args) {
 		//// Put your code here
+		int num = Integer.parseInt(args[0]);
+		int sum = 1;
+		String str = "1";
+		for (int i = 2; i<num; i++){
+			if (num % i == 0){
+				
+				sum = sum + i;
+			}
+		}
+		if (num != sum){
+			System.out.println(num + " is not a perfect number");
+		}
+		else{
+			for(int i = 2; i<num; i++){
+				if (num%i == 0){
+					str = str + (" + " + i  );
+				}
+			}
+			System.out.println(num + " is a perfect number since " +  num + " = " + str);
+		}
+
+
 	}
 }
