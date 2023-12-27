d8f1d27ad20885bd552bb79d84fcbf4713dd944d
BenS <bshalev1@gmail.com>
2023-12-27 16:58:32 +0200

    home work

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,5 +4,21 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		//// Put your code here
+		int num = Integer.parseInt(args[0]);
+		int numleft = num/2; 
+		for (int i = 1; i<=num; i++ ){
+			if (i % 2 == 1){
+				for (int n = 1; n<= num; n++){
+					System.out.print("* ");
+				}
+			}
+			else{
+				for (int n = 1; n<= num; n++){
+					System.out.print(" *");;
+				}
+			}
+			System.out.println();	
+
+		}
 	}
 }
