37aff276cdcebc847ce738771cfa0932eac749c3
BenS <bshalev1@gmail.com>
2023-12-27 15:09:58 +0200

    home work

--- Reverse.java
+++ Reverse.java
@@ -13,14 +13,14 @@ public class Reverse {
 		}
 		System.out.println(" ");
 		int x = name.length();		 // chek the num of the muddle char
-		if (name.length() % 2 == 1 ){
+		//if (name.length() % 2 == 1 ){
 			System.out.println("The middle character is " + name.charAt(x/2));		 // print the middle char
 
-		}
-		else{
-			System.out.print("The middle character is " + name.charAt((x/2)+1));
-			System.out.println(name.charAt(x/2));
-		}
+		//}
+		//else{
+		//	System.out.print("The middle character is " + name.charAt((x/2)+1));
+		//	System.out.println(name.charAt(x/2));
+		//}
 
 		//// Put your code here
 	}
